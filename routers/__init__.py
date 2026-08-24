8eb70c7d3ab6a05b7f3ee0281cab549d6452f7be
Kirk Alyn Santos <kirkalyn13@gmail.com>
2026-08-23 23:40:26 -0400

    fix: register health router

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from routers import scripts
+from routers import scripts, health
 
 def register_routers(app: FastAPI):
     """Register all defined Fast API routers here"""
     app.include_router(scripts.router)
+    app.include_router(health.router)
